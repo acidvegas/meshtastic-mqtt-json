8ff2e6b04ac48f9c35347564876f6f9ee3239638
acidvegas <acid.vegas@acid.vegas>
2024-11-29 23:05:19 -0500

    conflicting pypi repo names

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="meshtastic-mqtt-json",
-    version='0.1.1',
+    version='0.1.2',
     author='acidvegas',
     author_email='acid.vegas@acid.vegas',
     description='A lightweight Python library for parsing Meshtastic MQTT messages',
