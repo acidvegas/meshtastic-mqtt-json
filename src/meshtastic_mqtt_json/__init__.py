c1b8cbfa0be6037261eeec33a8e934f680fee1b3
acidvegas <acid.vegas@acid.vegas>
2024-12-02 21:25:33 -0500

    Prepare for 1.0.4

--- src/meshtastic_mqtt_json/__init__.py
+++ src/meshtastic_mqtt_json/__init__.py
@@ -4,6 +4,6 @@ Meshtastic MQTT Interface - A lightweight Python library for parsing Meshtastic 
 
 from .client import MeshtasticMQTT
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 __author__  = "acidvegas"
 __license__ = "ISC" 
